e0b56fdb481f8f18357268ab85eacbf3166de2d5
Salih Yılmaz <salih_yil@live.com>
2022-04-25 11:37:17 +0300

    remove useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 
 import Login from "./components/Login";
 import UserInfo from "./components/UserInfo";
