eef804dc9c68b7ff4fff11461820e39065a02245
Salih Yılmaz <salih_yil@live.com>
2022-04-26 14:59:45 +0300

    yup max func. removed

--- src/pages/Authentication/validate.js
+++ src/pages/Authentication/validate.js
@@ -4,6 +4,5 @@ export const VALIDATION_SCHEMA = Yup.object({
   email: Yup.string()
     .email("Invalid email address")
     .required("Email is required")
-    .max(17)
     .matches("sincere@april.biz", "Please enter a valid email"),
 });
