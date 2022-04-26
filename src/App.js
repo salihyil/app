070fb356ca09f708683d90b18b059124c48fb583
Salih Yılmaz <salih_yil@live.com>
2022-04-26 17:47:57 +0300

    css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Login, UserInfo, PostComments, PostDetail } from "./pages";
+import { Login, UserInfo, PostDetail } from "./pages";
 
 function App() {
   return (
@@ -8,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/postdetail/:id" element={<PostDetail />} />
-        <Route path="/postcomments/:id" element={<PostComments />} />
         <Route path="/userInfo" element={<UserInfo />} />
       </Routes>
     </BrowserRouter>
