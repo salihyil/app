4ee11dca7d8850258c6e091127725fbd0eb9b365
Salih Yılmaz <salih_yil@live.com>
2022-05-09 10:47:53 +0300

    userReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/user/userSlice";
+import userReducer from "../features/user/userSlice.js";
 
 export const store = configureStore({
   reducer: {
